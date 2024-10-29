2d647409e872d4976e8a873a28d0226ca1939112
Ron Hagafny <Ronhagafny20@gmail.com>
2024-10-29 10:31:36 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import './App.css'
+
+export default function App() {
+  return (
+    <div className="container mx-auto max-w-lg h-screen flex justify-center mt-4">
+      <h1 className="text-3xl font-bold">Hello World</h1>
+    </div>
+  )
+}
